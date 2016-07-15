c6d4c3c6dde3a15fe38c65cde74d92969c200fce
mattiab31 <mattiab31@gmail.com>
2016-07-15 12:31:35 +0200

    Update code.py

--- code.py
+++ code.py
@@ -2,20 +2,20 @@ import sys
 import os
 import telepot
 
-bot = telepot.Bot('153736884:AAGY5qfwDcQE21HCYWY1JftScSYWAv7Lqo8')
+bot = telepot.Bot('Api Key of your telegram bot')
 z= 1234
 i=0
 while 1:
 	if i == 3:
-		bot.sendMessage(-120421181, text="3 inserimenti errati")
-		os.system("sudo python /home/pi/tg/camerapycode.py")
+		bot.sendMessage(-120421181, text="3 Wrong accesses!")
+		os.system("sudo python /home/pi/tg/camerapycode.py") 
 		bot.sendPhoto(-120421181, photo=open('/home/pi/imagecode.jpg', 'rb'))
         	os.remove("/home/pi/imagecode.jpg")
 		i=0
 		break
-	x = input ("Immetti codice")
+	x = input ("Insert the secret code!")
 	if x == z :
-		os.system("sudo python /home/pi/tg/apriporta.py")
+		os.system("sudo python /home/pi/tg/apriporta.py") //script to open the door
 		bot.sendMessage(-120421181, text="Qualcuno ha inserito il codice esatto!")
 		i=0
 		print("eureka")
