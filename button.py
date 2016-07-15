60dd16b2d6bc3cc101e4cccf731ff965ed7129ff
mattiab31 <mattiab31@gmail.com>
2016-07-15 12:30:30 +0200

    Update button.py

--- button.py
+++ button.py
@@ -8,13 +8,13 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN)
-bot = telepot.Bot('153736884:AAGY5qfwDcQE21HCYWY1JftScSYWAv7Lqo8')
+bot = telepot.Bot('Api Key of your telegram bot')
 while True:
    input_state = GPIO.input(4)
    if input_state == False:
        print('button pressed')
        time.sleep(0.2)
-       os.system("sudo python /home/pi/tg/camerapy.py")
+       os.system("sudo python /home/pi/tg/camerapy.py") // script used to take the picture. Placed in folder subfiles
        bot.sendMessage(-120421181, text="button camera pressed")
        bot.sendPhoto(-120421181, photo=open('/home/pi/image.jpg', 'rb'))
        os.remove("/home/pi/image.jpg")
